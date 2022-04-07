45168a6b00759f75da216fc2b1cc439404294d0d
Matheus Araujo <matheus-araujo2001@hotmail.com>
2022-04-07 14:54:04 -0300

    Svg is not working

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      less: {
+        additionalData: `@import "@/assets/css/variables.less";`,
+      },
+    },
+  },
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule("svg");
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use("babel-loader")
+      .loader("babel-loader")
+      .end()
+      .use("vue-svg-loader")
+      .loader("vue-svg-loader");
+  },
+});
