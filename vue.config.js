a787d3e504857c315c369bec61fc687a7d35ffce
Matheus Araujo <matheus-araujo2001@hotmail.com>
2022-04-08 14:13:10 -0300

    Solved SVG error + create lateral menu

--- vue.config.js
+++ vue.config.js
@@ -8,16 +8,17 @@ module.exports = defineConfig({
       },
     },
   },
-  chainWebpack: (config) => {
-    const svgRule = config.module.rule("svg");
-
-    svgRule.uses.clear();
-
-    svgRule
-      .use("babel-loader")
-      .loader("babel-loader")
-      .end()
-      .use("vue-svg-loader")
-      .loader("vue-svg-loader");
+  chainWebpack: config => {
+    config.module.rules.delete("svg");
   },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.svg$/, 
+          loader: 'vue-svg-loader', 
+        },
+      ],
+    }      
+  }
 });
