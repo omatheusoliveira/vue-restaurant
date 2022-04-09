e72ae7e57eb596910b8a284eb7107ba5f17c137d
Matheus Araujo <matheus-araujo2001@hotmail.com>
2022-04-08 21:44:03 -0300

    Criando conteudo principal

---
+++ src/components/ItemsList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="items-list">
+    <ItemBurguer v-for="item in itemsList" :key="item.id" :item="item" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import ItemBurguer from "./Item";
+export default {
+  name: "ItemsList",
+  components: {
+    ItemBurguer,
+  },
+  data() {
+    return {
+      itemsList: [],
+    };
+  },
+  created() {
+    axios.get("http://localhost:3000/burguers").then((response) => {
+      this.itemsList = response.data;
+    });
+  },
+};
+</script>
+
+<style style="less" scoped>
+.items-list {
+  margin: 50px;
+  display: flex;
+}
+</style>
